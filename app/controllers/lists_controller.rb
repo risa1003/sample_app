f1e89ee47f99713a3befbfa9e72cbeeef2397caa
EC2 Default User <ec2-user@ip-172-31-10-127.ap-northeast-1.compute.internal>
2023-06-18 12:30:54 +0000

    [Update]投稿機能の編集・更新・削除・バリデーションの設定

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,11 +6,14 @@ class ListsController < ApplicationController
 
   def create
     #データを受け取り新規作成するためのインスタンス作成
-    list = List.new(list_params)
+    @list = List.new(list_params)
     #データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    #トップ画面へリダイレクト
-    redirect_to '/top'
+    if @list.save
+    #詳細画面へリダイレクト
+      redirect_to list_path(list.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -18,13 +21,29 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
   end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy #データ(レコード)を削除
+    redirect_to '/lists' #投稿一覧画面へリダイレクト
+  end
+
+
   private
   #ストロングパラメータ
   def list_params
-  params.require(:list).permit(:title, :body)
+  params.require(:list).permit(:title, :body, :image)
   end
 end
